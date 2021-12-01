2b452c069faa8cc61e444b270eb602e16ce7516e
Mark Ashton <meashton@gmail.com>
2021-12-01 09:32:53 -0600

    Day 1 part1 oneliner

--- day1/part1.rb
+++ day1/part1.rb
@@ -1,6 +1,5 @@
 def main
-  input = File.readlines('./input.txt')
-  puts input.map(&:to_i).each_cons(2).count { |x| x[1] > x[0] }
+  puts File.foreach('./input.txt').map(&:to_i).each_cons(2).count { |x| x[1] > x[0] }
 end
 
 main if __FILE__ == $PROGRAM_NAME
