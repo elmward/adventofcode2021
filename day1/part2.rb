af16620860500995753ddc34f909890544c21c27
Mark Ashton <meashton@gmail.com>
2021-12-01 09:59:25 -0600

    simplified comparison for part 2 -- h/t Brad Johnson aka 'The King of Excel'

--- day1/part2.rb
+++ day1/part2.rb
@@ -1,5 +1,5 @@
 def main
-  puts File.foreach('./input.txt').map(&:to_i).each_cons(4).count { |x| x[1..3].sum > x[0..2].sum }
+  puts File.foreach('./input.txt').map(&:to_i).each_cons(4).count { |x| x.last > x.first }
 end
 
 main if __FILE__ == $PROGRAM_NAME
