8659464b89fac2f2e25859b799d1a7e351727fef
Mark Ashton <meashton@gmail.com>
2021-12-08 20:00:27 -0600

    day 7 part 2

---
+++ day7/part2.rb
@@ -0,0 +1,15 @@
+def main
+  crabs = File.read('./input.txt').split(',').map(&:to_i)
+
+  best_position = (crabs.min..crabs.max).map do |pos|
+    fuel_consumption = crabs.sum do |crab|
+      distance = (crab - pos).abs
+      (1..distance).sum
+    end
+    [pos, fuel_consumption]
+  end.min_by { |pos, fuel| fuel }
+
+  puts best_position[1]
+end
+
+main if __FILE__ == $PROGRAM_NAME
